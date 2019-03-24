6aa26de992b50985b175fe8f38b67cfcc7588763
Pechenin.NA <n.pechenin@socialquantum.com>
2019-03-24 11:44:26 +0300

    add new script for open door

---
+++ Assets/Script/DeviceOperator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceOperator : MonoBehaviour {
+    public float radius = 1.5f; // Расстояние, с которого персонаж может активировать устройства. 
+
+    void Update() {
+        if (Input.GetButtonDown("Fire3")) {
+            // Реакция на кнопку ввода, заданную в настройках ввода в Unity. 
+
+            // Метод OverlapSphere() возвращает список ближайших объектов.
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+
+            foreach (Collider hitCollider in hitColliders) {
+                Vector3 direction = hitCollider.transform.position - transform.position;
+                
+                // Сообщение отправляется только при корректной ориентации персонажа.
+                if (Vector3.Dot(transform.forward, direction) > .5f) {
+                    // Метод SendMessage() пытается вызвать именованную функцию независимо от типа целевого объекта.
+                    hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/DoorOpenDevice.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenDevice : MonoBehaviour {
+    [SerializeField] private Vector3 dPos; // Смещение, применяемое при открывании двери.
+
+    private bool _open; // Переменная типа Boolean для слежения за открытым состоянием двери.
+
+    public void Operate() {
+        if (_open) {
+            // Открываем или закрываем дверь в зависимости от ее состояния.
+            Vector3 pos = transform.position - dPos;
+            transform.position = pos;
+        }
+        else {
+            Vector3 pos = transform.position + dPos;
+            transform.position = pos;
+        }
+
+        _open = !_open;
+    }
+}
