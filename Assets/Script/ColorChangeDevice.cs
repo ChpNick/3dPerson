f7ba8b7b568411152636154cc8af457af7859237
Pechenin.NA <n.pechenin@socialquantum.com>
2019-03-27 18:45:50 +0300

    add tv monitor

---
+++ Assets/Script/ColorChangeDevice.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class ColorChangeDevice : MonoBehaviour {
+    // Объявление метода с таким же именем, как в сценарии для двери
+    public void Operate() {
+        Color random = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        GetComponent<Renderer>().material.color = random;
+    }
+}
