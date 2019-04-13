ad845ca502bdd2cd9e57f249a6c8c2392e0aa618
Pechenin.NA <n.pechenin@socialquantum.com>
2019-04-13 21:40:31 +0300

    add sheaders

--- Assets/Script/OrbitCamera.cs
+++ Assets/Script/OrbitCamera.cs
@@ -8,11 +8,14 @@ public class OrbitCamera : MonoBehaviour {
 
     public float rotSpeed = 1.5f;
     private float _rotY;
+    private float _rotX;
     private Vector3 _offset;
 
     void Start() {
 //        Сохранение начального смещения между камерой и целью.
         _rotY = transform.eulerAngles.y;
+        _rotX = transform.eulerAngles.x;
+        
         _offset = target.position - transform.position;
     }
 
@@ -25,9 +28,10 @@ public class OrbitCamera : MonoBehaviour {
         else {
             // или быстрый поворот с помощью мыши. 
             _rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
+            _rotX += Input.GetAxis("Mouse Y") * rotSpeed * 3;
         }
         // Поддерживаем начальное смещение, сдвигаемое в соответствии с поворотом
-        Quaternion rotation = Quaternion.Euler(0, _rotY, 0);
+        Quaternion rotation = Quaternion.Euler(_rotX, _rotY, 0);
         transform.position = target.position - (rotation * _offset);
         
         // Камера всегда направлена на цель, где бы относительно этой цели она ни располагалась.
---
+++ Assets/Script/UI/WeatherController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+public class WeatherController : MonoBehaviour {
+    [SerializeField] private Material sky; // Ссылаться можно не только на объекты сцены, но и на материал на вкладке Project. 
+    [SerializeField] private Light sun; 
+    
+    private float _fullIntensity;
+    private float _cloudValue = 0f;
+    
+    void Start() {
+        _fullIntensity = sun.intensity; // Исходная интенсивность света считается «полной». 
+    }
+    void Update() {
+        SetOvercast(_cloudValue);
+        _cloudValue += .005f; // Для непрерывности перехода увеличивайте значение в каждом кадре. 
+    }
+    private void SetOvercast(float value) { // Корректируем как значение материала Blend, так и интенсивность света.
+            sky.SetFloat("_Blend", value); 
+            sun.intensity = _fullIntensity - (_fullIntensity * value);
+    }
+}
